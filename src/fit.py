445a7df3ff3ae1742d1de16120760c9dfcba988d
dlazzaretto <denise.lazzaretto@studenti.unipd.it>
2025-03-22 13:24:44 +0200

    Thanks CEO

--- src/fit.py
+++ src/fit.py
@@ -1,25 +1,42 @@
-"""Contains the necessary tools to fit a dataset."""
-
 import matplotlib.pyplot as plt
 import numpy as np
+import json
+from numpy.exceptions import VisibleDeprecationWarning
 from scipy.optimize import curve_fit
 
 # We need the value of the earth gravity to determine the initial velocity and initial angle
 from generate import EARTH_GRAVITY
 
 # Step 1: Determine the starting velocity and starting angle of the test dataset `data/A-test.txt`.
-
 ## 1. Load data file using https://numpy.org/doc/stable/reference/generated/numpy.loadtxt.html#numpy-loadtxt .
 ##    The first column corresponds to the abscissa x and the second column to the ordinate y.
+xdata, ydata = np.loadtxt("../data/A-test.txt", unpack=True)
+print(xdata, ydata)
 
 ## 2. Define the model: Look to the example on SciPy (the link below)
-
+def parabola(x, a, b):
+	return a*x+b*x**2
+	
 ## 3. Fit using https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html#curve-fit
+res, cov = curve_fit(parabola, xdata, ydata)
+print("a:", res[0])
+print("b:", res[1])
 
 ## 4. Calculate the initial velocity and initial angle from the model parameters.
 ##    (Yes, this is a math exercise ;-) ) Compare your formulae with the Programmer.
+print("Initial angle:" , np.tan(res[0]))
+print("Initial velocity:" , np.sqrt(EARTH_GRAVITY/(-2*res[1])))
+
 
 # Step 2: Plot the data and your fit into one diagram.
+plt.plot(xdata, ydata, marker='o')
+xpoints = np.linspace(0, 10, 200)
+plt.plot(xpoints, parabola(xpoints, *res), label="fit");
+plt.xlabel("x [a. u.]")
+plt.ylabel("y [a. u.]")
+plt.title("Ski jumper trajectory")
+plt.show()
+
 
 ## The most popular library for making plots is matplotlib:
 ## Homepage: https://matplotlib.org/
